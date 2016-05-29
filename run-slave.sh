aac506808f94bf24c90033fe051c62c94ffb29a2
Blair MacNeil <macneib@gmail.com>
2016-05-29 08:31:00 -0400

    Update run-slave.sh to use wget instead of curl

--- run-slave.sh
+++ run-slave.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -O http://${MASTER_HOST}:8080/jnlpJars/slave.jar
+wget http://${MASTER_HOST}:8080/jnlpJars/slave.jar
 java -jar slave.jar -jnlpUrl http://${MASTER_HOST}:8080/computer/${NODE}/slave-agent.jnlp
