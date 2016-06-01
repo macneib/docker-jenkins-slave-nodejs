54c98e9a17ce9424f919218bfba05d3696d11dfc
Blair MacNeil <macneib@gmail.com>
2016-06-01 16:48:46 -0400

    fixed typo

--- run-slave.sh
+++ run-slave.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget {$PROTOCOL}://${USER}:${PASSWORD}@${MASTER_HOST}/jnlpJars/slave.jar
-java -jar slave.jar -jnlpUrl {$PROTOCOL}://${USER}:${PASSWORD}@${MASTER_HOST}/computer/${NODE}/slave-agent.jnlp -secret ${SECRET}
+wget ${PROTOCOL}://${USER}:${PASSWORD}@${MASTER_HOST}/jnlpJars/slave.jar
+java -jar slave.jar -jnlpUrl ${PROTOCOL}://${USER}:${PASSWORD}@${MASTER_HOST}/computer/${NODE}/slave-agent.jnlp -secret ${SECRET}
